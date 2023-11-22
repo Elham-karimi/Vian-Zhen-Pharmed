881be5539f51366e7aaedd6bb482b62d074afb7c
Elham Karimi <elhamka636@gmail.com>
2023-11-22 15:01:23 +0330

    Write UserService in my app

--- client/src/app/services/user.service.ts
+++ client/src/app/services/user.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../models/user.model';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+
+  getAllUsers() : void {
+    this.http.get<User[]>('https://localhost:5001/api/user').pipe(
+      map.
+    )
+    
+  } 
 }
