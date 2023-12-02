6a7d11e36b51417ca4b55c2427f1ab29312ddf71
Elham Karimi <elhamka636@gmail.com>
2023-12-02 14:01:10 +0330

    Password Salt & Hash Added to AppUser

--- api/Models/AppUser.cs
+++ api/Models/AppUser.cs
@@ -3,7 +3,7 @@ namespace api.Models;
 public record AppUser(
     [property: BsonId, BsonRepresentation(BsonType.ObjectId)] string? Id,
     string Email,
-    string Password,
-    string ConfirmPassword,
+    byte[] PasswordSalt,
+    byte[] PasswordHash,
     City City
 );
