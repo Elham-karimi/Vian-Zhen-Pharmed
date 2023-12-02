fd8edb59abb4ae02e9c976f0b79d8f4ae2e737b8
Elham Karimi <elhamka636@gmail.com>
2023-12-02 14:02:17 +0330

    Product Model Updated

--- api/Models/Product.cs
+++ api/Models/Product.cs
@@ -2,14 +2,12 @@ namespace api.Models;
 
 public record Product(
     [property: BsonId, BsonRepresentation(BsonType.ObjectId)] string? Id,
-    [MinLength(1),MaxLength(100)] string Name,
-    [MinLength(1),MaxLength(1000)] string ShortDescription,
-    SpecificSpecification SpecificSpecification
-);
-public record SpecificSpecification(
-    [MinLength(1),MaxLength(300)] string ProductType,
-    [MinLength(1),MaxLength(300)] string ConsumerGroup,
-    [MinLength(1),MaxLength(1000)] string UsageCases,
-    [MinLength(1),MaxLength(300)] string Dosage,
-    [MinLength(1),MaxLength(2000)] string Combination
-);
+    string PersianName,
+    string EnglishName,
+    string ShortDescription,
+    string UsageCases,
+    string ProductType,
+    string ConsumerGroup,
+    string Dosage,
+    Combination Combination
+    );
