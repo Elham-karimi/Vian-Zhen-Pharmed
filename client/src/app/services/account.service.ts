320cf5e2112ab200999c609ac3669a06895c4bbc
Elham Karimi <elhamka636@gmail.com>
2023-12-02 14:06:45 +0330

    AccountService is Added to this Project

--- client/src/app/services/account.service.ts
+++ client/src/app/services/account.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, map } from 'rxjs';
+import { User } from '../models/user.model';
+import { RegisterUser } from '../models/register-user.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
 
-  constructor() { }
+  constructor(private http : HttpClient ) {}
+
+  registerUser(userInput : RegisterUser) : Observable<User> {
+    return this.http.post<User> ('https://localhost:5001/api/account/register', userInput).pipe(
+      map(user => user)
+    );
+  }
+
 }
