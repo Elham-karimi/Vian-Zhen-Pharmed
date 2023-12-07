db629d60055aa5f0c31738670280c15f15d79383
Elham Karimi <elhamka636@gmail.com>
2023-12-07 20:31:44 +0330

    I dont need Admin Model at this project,Cleared It

--- api/Interfaces/IAdminRepository.cs
+++
@@ -1,8 +0,0 @@
-namespace api.Interfaces;
-
-public interface IAdminRepository
-{
-    public Task<List<UserDto>> GetAllAsync(CancellationToken cancellationToken);
-
-    public Task<UserDto?> GetَUserbyIdAsync(string userId, CancellationToken cancellationToken);
-}
