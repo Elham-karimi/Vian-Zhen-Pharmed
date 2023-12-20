93315e2fe47e7719baf975e9c9ad0acf73bc1491
Elham Karimi <elhamka636@gmail.com>
2023-12-20 18:47:18 +0330

    navbar.component.html,scss,ts Updated

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconRegistry} from '@angular/material/icon';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconRegistry } from '@angular/material/icon';
+import { AccountService } from 'src/app/services/account.service';
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-navbar',
@@ -10,6 +12,11 @@ import {MatIconRegistry} from '@angular/material/icon';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
-  
+user : User | null | undefined;
 
+  constructor(private accountService : AccountService) {
+    accountService.currentUser$.subscribe({
+      next: response => this.user = response
+    })
+  }
 }
