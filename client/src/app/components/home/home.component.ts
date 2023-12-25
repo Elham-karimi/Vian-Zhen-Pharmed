c93288a173adef63b4d9ed3db5a8facd43ed79e6
Elham Karimi <elhamka636@gmail.com>
2023-12-25 17:16:41 +0330

    home.component.ts equipped with new Logout Method

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user.model';
+import { AccountService } from 'src/app/services/account.service';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -7,14 +8,26 @@ import { UserService } from 'src/app/services/user.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
-  allUsers : User[] | undefined;
-  constructor(private userService : UserService){}
- 
-  showAllUsers(){
+export class HomeComponent implements OnInit {
+  allUsers: User[] | undefined;
+  user: User | null | undefined;
+
+  constructor(private userService: UserService, private accountService: AccountService) { }
+
+  ngOnInit(): void {
+   this.accountService.currentUser$.subscribe({
+      next: res => this.user = res
+    })
+  }
+
+  showAllUsers() {
     return this.userService.getAllUsers().subscribe({
-      next : users => this.allUsers = users,
-      error : err => console.log(err)      
-     });
+      next: users => this.allUsers = users,
+      error: err => console.log(err)
+    });
+  }
+
+  logout(): void {
+    this.accountService.logoutUser();
   }
 }
