da2fe6377ec8673eb6feb64d023e33f8dca639cf
Elham Karimi <elhamka636@gmail.com>
2023-12-25 17:17:39 +0330

    Equipped with ngoninit & Logout Methods

--- client/src/app/components/navbar/navbar.component.ts
+++ client/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -11,12 +11,18 @@ import { User } from 'src/app/models/user.model';
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
 user : User | null | undefined;
 
-  constructor(private accountService : AccountService) {
-    accountService.currentUser$.subscribe({
+  constructor(private accountService : AccountService) {}
+
+  ngOnInit(): void {
+   this.accountService.currentUser$.subscribe({
       next: response => this.user = response
     })
+  }
+
+  logout(): void {
+    this.accountService.logoutUser();
   }
 }
