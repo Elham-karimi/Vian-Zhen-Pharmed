2a621f9e7b1d05f8482403e6642d3361d9a02297
Elham Karimi <elhamka636@gmail.com>
2023-12-25 14:49:01 +0330

    Program.cs was House keeping

--- api/Program.cs
+++ api/Program.cs
@@ -1,72 +1,16 @@
 
-using System.Text;
-using api.Services;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
-#region MongoDbSettings
-///// get values from this file: appsettings.Development.json /////
-// get section
-builder.Services.Configure<MongoDbSettings>(builder.Configuration.GetSection(nameof(MongoDbSettings)));
-
-// get values
-builder.Services.AddSingleton<IMongoDbSettings>(serviceProvider =>
-serviceProvider.GetRequiredService<IOptions<MongoDbSettings>>().Value);
-
-// get connectionString to the db
-builder.Services.AddSingleton<IMongoClient>(serviceProvider =>
-{
-    MongoDbSettings uri = serviceProvider.GetRequiredService<IOptions<MongoDbSettings>>().Value;
-
-    return new MongoClient(uri.ConnectionString);
-});
-
-#endregion MongoDbSettings
-
-#region Cors: baraye ta'eede Angular HttpClient requests
-builder.Services.AddCors(options =>
-    {
-        options.AddDefaultPolicy(policy =>
-            policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
-    });
-#endregion Cors
-
-#region Authentication & Authorization
-string tokenValue = builder.Configuration["TokenKey"]!;
-
-if (!string.IsNullOrEmpty(tokenValue))
-{
-    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-        .AddJwtBearer(options =>
-        {
-            options.TokenValidationParameters = new TokenValidationParameters
-            {
-                ValidateIssuerSigningKey = true,
-                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(tokenValue)),
-                ValidateIssuer = false,
-                ValidateAudience = false
-            };
-        });
-}
-#endregion Authentication & Authorization
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
 
-# region Dependency Injection
+builder.Services.AddApplicationService(builder.Configuration);
+builder.Services.AddIdentityService(builder.Configuration);
+builder.Services.AddRepositoryServices();
 
-builder.Services.AddScoped<IAccountRepository , AccountRepository>();
-builder.Services.AddScoped<IProductRepository , ProductRepository>();
-builder.Services.AddScoped<ICityRepository , CityRepository>();
-builder.Services.AddScoped<IUserRepository , UserRepository>();
-builder.Services.AddScoped<ITokenService, TokenService>();
-
-#endregion Dependency Injection
 
 var app = builder.Build();
 
