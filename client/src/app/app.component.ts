ffa431dec160a28a579805805a1018218b1276fc
Elham Karimi <elhamka636@gmail.com>
2023-12-25 17:13:00 +0330

    Add a Method to Give Response from API & Throw it to AccountService

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Title, Meta } from '@angular/platform-browser';
+import { User } from './models/user.model';
+import { AccountService } from './services/account.service';
 // import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
@@ -8,18 +9,20 @@ import { Title, Meta } from '@angular/platform-browser';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  constructor(private metaTagService: Meta) {}
-  ngOnInit() {
-    this.metaTagService.addTags([
-      {
-        name: 'keywords',
-        content: 'Angular SEO Integration, Music CRUD, Angular Universal',
-      },
-      { name: 'robots', content: 'index, follow' },
-      { name: 'author', content: 'Digamber Singh' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'date', content: '2019-10-31', scheme: 'YYYY-MM-DD' },
-      { charset: 'UTF-8' },
-    ]);
+  constructor(private accountService: AccountService) { }
+  
+  ngOnInit(): void {
+    this.getLocalStorageCurrentValues();
+  }
+ 
+  getLocalStorageCurrentValues(): void {
+    const userString: string | null = localStorage.getItem('user');
+
+    if (userString) {
+      const user: User = JSON.parse(userString);
+
+      this.accountService.setCurrentUser(user);
+    }
+
   }
 }
