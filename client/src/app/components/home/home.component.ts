c19d9e190d771a2bea63a8731f041d789744edc0
Elham Karimi <elhamka636@gmail.com>
2023-12-30 14:56:49 +0330

    Implement ngOnDestroy for home.component.ts

--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import { User } from 'src/app/models/user.model';
 import { AccountService } from 'src/app/services/account.service';
 import { UserService } from 'src/app/services/user.service';
@@ -8,16 +9,14 @@ import { UserService } from 'src/app/services/user.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnDestroy{
   allUsers: User[] | null | undefined;
-  user: User | null | undefined;
+  subscription : Subscription | undefined;
 
   constructor(private userService: UserService, private accountService: AccountService) { }
 
-  ngOnInit(): void {
-   this.accountService.currentUser$.subscribe({
-      next: res => this.user = res
-    })
+  ngOnDestroy(): void {
+      this.subscription?.unsubscribe();
   }
 
   showAllUsers() {
